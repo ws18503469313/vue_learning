5dcc3d501e8fe4fe1c3fcf42a91fe68ef8e384bc
wangshuai <wangshuai@songyekj.com>
2019-09-05 15:25:56 +0800

    模板

--- webpack-test/src/template/app.vue
+++ webpack-test/src/template/app.vue
@@ -1,36 +1,12 @@
 <template>
     <div>
         <router-view></router-view>
-        <div>Hello{{name}}</div>
-        <div>
-            <v-title title="Vue组件化" ></v-title>
-            <v-button @click="handleClick" >点击按钮</v-button>
-        </div>
-        <p>
-            <img src="../images/1567479808(1).jpg" style="width: 200px;" />
-        </p>
+<!--        <div>Hello{{name}}</div>-->
     </div>
 </template>
 <script>
-    import vTitle from './title.vue'
-    import vButton from  './button.vue'
-
     export default{
-        data() {
-            return {
-                name: 'Vue.js'
-            }
-        },
-        components: {
-            vTitle,
-            vButton
-        },
-        methods: {
-            handleClick(e){
-                // console.log('parent component event')
-                console.log(e)
-            }
-        }
+
     }
 </script>
 <style scoped >
